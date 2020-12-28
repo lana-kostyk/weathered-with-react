e56cce90f30fe15952bccb532549ab1d12e4e660
lana-kostyk <70900357+lana-kostyk@users.noreply.github.com>
2020-12-28 22:19:02 +0100

    added rel to a link in footer

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weathered</h1>
-      <footer>Open sourced on {" "}<a href="https://github.com/lana-kostyk/weathered-with-react" target="_blank">github</a> by Svitlana Kostyk</footer> 
+      <footer>Open sourced on {" "}<a href="https://github.com/lana-kostyk/weathered-with-react" target="_blank" rel="noreferrer">github</a> by Svitlana Kostyk</footer> 
     </div>
   );
 }
