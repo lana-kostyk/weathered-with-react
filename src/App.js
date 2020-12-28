32c81ae90b32d94c04ff2247eef82bae18f88720
lana-kostyk <70900357+lana-kostyk@users.noreply.github.com>
2020-12-28 22:07:40 +0100

    added a footer with a link to git

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-     <h1>Weathered</h1> 
+      <h1>Weathered</h1>
+      <footer>Open sourced on {" "}<a href="https://github.com/lana-kostyk/weathered-with-react" target="_blank">github</a> by Svitlana Kostyk</footer> 
     </div>
   );
 }
