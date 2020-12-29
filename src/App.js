2ff2ef72882ffbf97818c084ed4c73221292efc2
lana-kostyk <70900357+lana-kostyk@users.noreply.github.com>
2020-12-29 13:56:36 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weathered</h1>
       <footer>Open sourced on {" "}<a href="https://github.com/lana-kostyk/weathered-with-react" target="_blank" rel="noreferrer">github</a> by Svitlana Kostyk</footer> 
+     </div>
     </div>
   );
 }
